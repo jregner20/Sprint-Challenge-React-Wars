83c75b006f0196831c3ec2f866bd0f8891cba89b
Jake Regner <jregner20@gmail.com>
2019-11-08 11:39:05 -0500

    Add state to App

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  const [data, setData] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
